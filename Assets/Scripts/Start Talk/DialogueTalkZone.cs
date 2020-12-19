23c9cd900244bebb4a2b72bfd4b6feaf9a45ebe6
kasperfly <43131040+kasperfly@users.noreply.github.com>
2020-12-20 00:25:37 +0100

    0.3

---
+++ Assets/Scripts/Start Talk/DialogueTalkZone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTalkZone : MonoBehaviour
+{
+    [SerializeField] private GameObject speechBubble;
+    [SerializeField] private KeyCode talkKey = KeyCode.E;
+    [SerializeField] private Text keyInputText;
+
+    private void Awake()
+    {
+        speechBubble.SetActive(false);
+        keyInputText.text = talkKey.ToString();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(talkKey) && speechBubble.activeSelf)
+        {
+            // TODO: Start Dialogue.
+            Debug.Log("Start Dialouge");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            speechBubble.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            speechBubble.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/FaceTheCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTheCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        transform.LookAt(mainCamera.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
