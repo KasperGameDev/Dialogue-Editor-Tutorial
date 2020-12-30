054af5bb80f5f655b0738b9cbfbe2b2d5b37af8c
KasperGameDev <43131040+kasperfly@users.noreply.github.com>
2020-12-30 18:20:46 +0100

    save

--- Assets/Scripts/Dialogue Editor/Runtime/Scriptable Objects/DialogueContainerSO.cs
+++ Assets/Scripts/Dialogue Editor/Runtime/Scriptable Objects/DialogueContainerSO.cs
@@ -8,8 +8,69 @@ using UnityEngine.UIElements;
 [System.Serializable]
 public class DialogueContainerSO : ScriptableObject
 {
+    public List<NodeLinkData> NodeLinkDatas = new List<NodeLinkData>();
 
+    public List<DialogueNodeData> DialogueNodeDatas = new List<DialogueNodeData>();
+    public List<EndNodeData> EndNodeDatas = new List<EndNodeData>();
+    public List<StartNodeData> StartNodeDatas = new List<StartNodeData>();
+    public List<EventNodeData> EventNodeDatas = new List<EventNodeData>();
 
+    public List<BaseNodeData> AllNodes
+    {
+        get
+        {
+            List<BaseNodeData> tmp = new List<BaseNodeData>();
+            tmp.AddRange(DialogueNodeDatas);
+            tmp.AddRange(EndNodeDatas);
+            tmp.AddRange(StartNodeDatas);
+            tmp.AddRange(EventNodeDatas);
+
+            return tmp;
+        }
+    }
+}
+
+[System.Serializable]
+public class NodeLinkData
+{
+    public string BaseNodeGuid;
+    public string TargetNodeGuid;
+}
+
+[System.Serializable]
+public class BaseNodeData
+{
+    public string NodeGuid;
+    public Vector2 Position;
+}
+
+[System.Serializable]
+public class DialogueNodeData : BaseNodeData
+{
+    public List<DialogueNodePort> DialogueNodePorts;
+    public Sprite Sprite;
+    public DialogueFaceImageType DialogueFaceImageType;
+    public List<LanguageGeneric<AudioClip>> AudioClips;
+    public string Name;
+    public List<LanguageGeneric<string>> TextType;
+}
+
+[System.Serializable]
+public class EndNodeData : BaseNodeData
+{
+    public EndNodeType EndNodeType;
+}
+
+[System.Serializable]
+public class StartNodeData : BaseNodeData
+{
+
+}
+
+[System.Serializable]
+public class EventNodeData: BaseNodeData
+{
+    public DialogueEventSO DialogueEventSO;
 }
 
 [System.Serializable]
