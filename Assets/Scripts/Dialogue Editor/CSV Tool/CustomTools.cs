880aa679436166850846ee089cc4cf2c38432acb
KasperGameDev <43131040+kasperfly@users.noreply.github.com>
2021-01-21 23:11:32 +0100

    Save CSV

--- Assets/Scripts/Dialogue Editor/CSV Tool/CustomTools.cs
+++ Assets/Scripts/Dialogue Editor/CSV Tool/CustomTools.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public class CustomTools 
 {
+    [MenuItem("Custom Tools/Dialogue/Save to CSV")]
+    public static void SaveToCSV()
+    {
+        SaveCSV saveCSV = new SaveCSV();
+        saveCSV.Save();
+
+        EditorApplication.Beep();
+        Debug.Log("<color=green> Save CSV File successfully! </color>");
+    }
+
+
     [MenuItem("Custom Tools/Dialogue/Update Dialogue Languages")]
     public static void UpdataDialogueLanguage()
     {
